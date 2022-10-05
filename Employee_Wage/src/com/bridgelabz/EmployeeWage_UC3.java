fabd3cc256f9172167dd7e324340c3d366a74352
shubham <shubham.bondre.1995@gmail.com>
2022-10-05 23:07:20 +0530

    Part Time Employee And Wages

--- Employee_Wage/src/com/bridgelabz/EmployeeWage_UC3.java
+++ Employee_Wage/src/com/bridgelabz/EmployeeWage_UC3.java
@@ -1,12 +1,12 @@
 package com.bridgelabz;
 
-public class EmployeeWage_UC2 {
-    public static void main(String[] args) {
+public class EmployeeWage_UC3 {
+    public  static void main(String[] args) {
         int WAGE_PER_HOUR = 20;
-        System.out.println("Welcome to Employee Wage");
+        System.out.println("Welcome to Part Time Employee Wage");
         int randomVal = (int) Math.floor(Math.random() * 10) % 2;
         if (randomVal == 1) {
-            int empHrs = 8;
+            int empHrs = 4;
             int empWage = WAGE_PER_HOUR * empHrs;
             System.out.println("Emp Wage:" + empWage);
             System.out.println("Employee is Present");
